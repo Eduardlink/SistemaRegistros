a0e6fbba3e186e9335628890ec40176b18c562f2
Eduardo <epila8661@uta.edu.ec>
2022-06-25 17:05:00 -0500

    "Se establecio exitosamente la conexion con servidor remoto SQLServer"

---
+++ src/conexionBD/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Pc
+ */
+public class Conexion {
+    public static Connection getConexion(){
+        String conexionURL = "jdbc:sqlserver://mysqlregistros.database.windows.net:1433;database=SistemaRegistros;user=Edu@mysqlregistros;password=Tictactoe2001;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        
+        try{
+            Connection con = DriverManager.getConnection(conexionURL);
+            return con;
+        }catch(Exception ex){
+            System.out.println(ex);
+            return null;
+        }
+    }
+}
