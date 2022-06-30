e9eebe69274c6bc01ba94e36c7bbb641b17598fe
Eduardo <epila8661@uta.edu.ec>
2022-06-30 00:55:34 -0500

    Creacion e implementacion controlador loguin

--- src/paneles/admin.java
+++ src/paneles/admin.java
@@ -5,6 +5,7 @@
  */
 package paneles;
 
+import controladores.LoguinController;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,6 +19,18 @@ public class admin extends javax.swing.JPanel {
      */
     public admin() {
         initComponents();
+    }
+    
+        public void iniciarSesion(){
+        LoguinController controlador = new LoguinController();
+        boolean[] verificado = controlador.verificar("uuario", "clave", "1");
+        if (verificado[0] == false) {
+            JOptionPane.showMessageDialog(null, "No existe el usuario");
+        } else if (verificado[0] == true && verificado[1] == false) {
+            JOptionPane.showMessageDialog(null, "Contraseña incorrecta intente de nuevo");
+        } else if (verificado[0] == true && verificado[1] == true) {
+            JOptionPane.showMessageDialog(null, "Exito");
+        }
     }
 
     /**
