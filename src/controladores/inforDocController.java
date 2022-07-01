26aba50a946493a95a28f2e13b728960cbdbdf6a
Eduardo <epila8661@uta.edu.ec>
2022-07-01 18:37:38 -0500

    Clase base BusquedaPorFecha

--- src/controladores/inforDocController.java
+++ src/controladores/inforDocController.java
@@ -4,19 +4,36 @@
  */
 package controladores;
 
+import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
+import modelos.DataManager;
 
 /**
  *
  * @author Pc
  */
 public class inforDocController {
-    public String[] mostrarDatos(String cedula){
-        //[usuario,nombre,apellido,cedula]
-        return null;
+
+    public String[] mostrarDatos(String cedula) {
+        DataManager manejador = new DataManager();
+        String sql = "SELECT usuario,nombre,apellido,cedula FROM usuarios WHERE cedula ='" + cedula + "';";
+        ArrayList<Object> datos = new ArrayList<>();
+        datos = manejador.resultado(sql);
+        String[] info = {
+            datos.get(0).toString(),
+            datos.get(1).toString(),
+            datos.get(2).toString(),
+            datos.get(3).toString(),};
+        return info;
     }
-    
-    public DefaultTableModel cargarTabla(){
+
+    public DefaultTableModel cargarTabla() {
+        DataManager manejador = new DataManager();
+        String[] titulos ={
+           "Fecha","Entrada Matutina","Salida Matutina","Entrada Vespertina","Salida Vespertina","Horas" 
+        } ;
+        DefaultTableModel modeloTabla = new DefaultTableModel(null,titulos);
+        
         return null;
     }
 }
